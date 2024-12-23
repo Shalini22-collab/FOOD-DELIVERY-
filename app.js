48965f4f793d2c8452f0a6ca42d1de8d16717faa
Shalini22-collab <Shalini.2426mca1999@kiet.edu>
2024-12-23 14:23:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const cors = require("cors")
+const path = require('path')
+
+
+const app = express();
+const port = 5000;
+
+
+
+
+app.use(cors())
+// require('./models/allBLogs')
+require('./models/user')
+require('./models/items')
+require('./models/customers')
+
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/activity'))
+
+
+
+app.listen(port , () => {
+    console.log("Server is ruuning on " + port)
+})
+
+
+
+
+const mongoose = require("mongoose");
+const {mongoUrl} = require("./keys");
+
+mongoose.connect(mongoUrl);
+
+mongoose.connection.on("connected" , () => {
+    console.log("Connected to MongoDB")
+})
+
+mongoose.connection.on("error" , () => {
+    console.log("Not Connected to mongDB")
+}) 
---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports={
+    mongoUrl : "mongodb+srv://shubhranshu1924patel:cpRSbUbRhBaX6WGB@cluster0.zaspc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+     
+    Jwt_secret : "sfkjsdhfkjskjfh"
+
+    
+};
